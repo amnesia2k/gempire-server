d2e27ff3ba061214f8a723b6a6b37b2c6d0164cc
olathedev <tilewaolatoye17@gmail.com>
2025-07-17 09:17:40 +0100

    removed hard-coded 8000

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import { sql } from "drizzle-orm";
 import logger from "./utils/logger";
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 // 🧭 Fix __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
