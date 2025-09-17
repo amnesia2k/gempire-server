70336796867f3a2856bb9d2b147a08cbdda9e3de
olathedev <tilewaolatoye17@gmail.com>
2025-09-17 19:06:32 +0100

    feat: health

--- src/routes/health-route.ts
+++ src/routes/health-route.ts
@@ -3,6 +3,10 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
+  res.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
   res.json({
     status: "success",
     message: "🧠 Gempire backend is alive!",
